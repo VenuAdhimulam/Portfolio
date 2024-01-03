dec4300bd52938abec5b629cb682e1895dcecf29
Venu Adimulam <103015116+VenuAdhimulam@users.noreply.github.com>
2024-01-03 15:00:02 -0500

    Domain Name change test

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,7 +10,6 @@ import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import { experiences } from "../data";
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
